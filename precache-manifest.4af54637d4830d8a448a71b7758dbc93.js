0bdfed961b79894670aff67865bef461bbddac18
arseniyT <30984464+arseniyT@users.noreply.github.com>
2018-12-25 16:14:59 +0300

    Updates

---
+++ precache-manifest.4af54637d4830d8a448a71b7758dbc93.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fefd5a42a85868b0fdb0",
+    "url": "/hw_ts-react-news-api/static/js/runtime~main.fefd5a42.js"
+  },
+  {
+    "revision": "89a5cc24dd4cb22114f0",
+    "url": "/hw_ts-react-news-api/static/js/main.89a5cc24.chunk.js"
+  },
+  {
+    "revision": "03783d836442911ca812",
+    "url": "/hw_ts-react-news-api/static/js/1.03783d83.chunk.js"
+  },
+  {
+    "revision": "89a5cc24dd4cb22114f0",
+    "url": "/hw_ts-react-news-api/static/css/main.69e88d8e.chunk.css"
+  },
+  {
+    "revision": "337b36b4d1879e1390d739ee11fc7675",
+    "url": "/hw_ts-react-news-api/index.html"
+  }
+];
